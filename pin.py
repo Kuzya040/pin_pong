22073e9d8282afe6195e77c85e92ee11c38952f6
Kuzya040 <123809596+Kuzya040@users.noreply.github.com>
2023-05-19 18:01:22 +0300

    Update pin.py

--- pin.py
+++ pin.py
@@ -4,13 +4,20 @@ clock = time.Clock()
 image1 = image.load('pngwing.com (2).png')
 image2 = image.load('pngwing.com (4).png')
 image3 = image.load('platform.png')
-win = display.set_mode((750,500))
-bk = transform.scale(image2,(750,500))
+image4 = image.load('platform.png')
+image4 = transform.rotate(image4,180)
+win = display.set_mode((950,650))
+bk = transform.scale(image2,(950,650))
 display.set_caption("pin pong") #?   название программы
 font.init()
 text = font.SysFont('Arial',36)
-speed = randint(1,5)
+speed_x = 5
+speed_y = 5
+text  = font.SysFont('arial',40)
+loser = text.render('игрок 1 проиграл',1,(255,255,255))
+loser1 = text.render('игрок 2 проиграл',1,(255,255,255))
 game = False
+finish = False
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image,player_x, player_y,size_x,size_y,player_speed):
         super().__init__()
@@ -27,24 +34,40 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.x >5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < 750 - 80:
+        if keys[K_DOWN] and self.rect.x < 950 - 80:
             self.rect.y += self.speed
     def update_right(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.x >0:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < 750 - 80:
+        if keys[K_s] and self.rect.x < 950 - 80:
             self.rect.y += self.speed
-platform_left = Player(image3,1,300,120,220,5)
-platform_rigth = Player(image3,650,300,120,220,5)
+platform_left = Player(image3,1,300,70,220,5)
+platform_rigth = Player(image4,850,300,70,220,5)
+ball = GameSprite(image1,400,250,100,100,0)
 while not game:
     for i in event.get():
         if i.type == QUIT:
             game = True
-    win.blit(bk,(0,0))
-    platform_left.update_right()
-    platform_rigth.update_left()
-    platform_left.reset()
-    platform_rigth.reset()
+    if not finish:
+        win.blit(bk,(0,0))
+        platform_left.update_right()
+        platform_rigth.update_left()
+        platform_left.reset()
+        platform_rigth.reset()
+        ball.reset()
+        if finish != 1:
+            ball.rect.x +=speed_x
+            ball.rect.y +=speed_y     
+        if ball.rect.y <0 or ball.rect.y == 550:
+            speed_y *= -1
+        if sprite.collide_rect(platform_left,ball) or sprite.collide_rect(platform_rigth,ball):
+            speed_x *= -1
+        if ball.rect.x <0:
+            win.blit(loser,(225,325))
+            finish = True
+        if ball.rect.x > 870:
+            win.blit(loser1,(225,325))
+            finish = True
     display.update()
     time.delay(20)
